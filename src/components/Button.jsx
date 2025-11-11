58c5f0c814c4ca902b3efca6d249171200e64759
Pavan <kolipaka.pavan.kp@gmail.com>
2025-11-12 04:07:15 +0530

    contactMe_Scroll_Fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,39 @@
-const Button = ({ name, isBeam = false, containerClass }) => {
+const Button = ({ name, isBeam = false, containerClass = '', to, onClick }) => {
+  const handleClick = (e) => {
+    // If `to` is an in-page hash, perform smooth scrolling
+    if (to && typeof to === 'string' && to.startsWith('#')) {
+      // Prevent default in case this is used on an anchor or button
+      e && e.preventDefault && e.preventDefault();
+      const id = to.slice(1);
+      const el = document.getElementById(id) || document.querySelector(to);
+      if (el && typeof el.scrollIntoView === 'function') {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+
+    // Call any provided onClick handler as well
+    if (onClick && typeof onClick === 'function') {
+      onClick(e);
+    }
+  };
+
+  // If `to` is an external link (http / mailto), render an anchor
+  if (to && (to.startsWith('http') || to.startsWith('mailto:'))) {
+    return (
+      <a href={to} className={`btn ${containerClass}`}>
+        {isBeam && (
+          <span className="relative flex h-3 w-3">
+            <span className="btn-ping"></span>
+            <span className="btn-ping_dot"></span>
+          </span>
+        )}
+        {name}
+      </a>
+    );
+  }
+
   return (
-    <button className={`btn ${containerClass}`}>
+    <button onClick={handleClick} className={`btn ${containerClass}`}>
       {isBeam && (
         <span className="relative flex h-3 w-3">
           <span className="btn-ping"></span>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -121,7 +121,7 @@ const About = () => {
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
               <p className="grid-subtext">I&apos;m based in Hyderabad, India and open to work worldwide.</p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" to="#contact" />
             </div>
           </div>
         </div>
